a1409831449bc96f444610e3b8edf28b8c09536b
arrowheaddigitalagency-cmyk <293974959+arrowheaddigitalagency-cmyk@users.noreply.github.com>
2026-09-19 20:25:18 +0500

    feat: add Google Tag Manager (GTM) to head and noscript iframe to body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -191,6 +191,19 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${poppins.variable} ${montserrat.variable} font-sans`}>
       <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5D44Z56L');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <Script
           strategy="afterInteractive"
           src="https://www.googletagmanager.com/gtag/js?id=G-BDJJ5QD8LD"
@@ -213,6 +226,16 @@ export default function RootLayout({
         />
       </head>
       <body className="min-h-screen flex flex-col bg-[#F8FAFC] text-slate-900 selection:bg-[#0052EA] selection:text-white">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5D44Z56L"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <SmoothScroll>{children}</SmoothScroll>
       </body>
     </html>
